02470e21241a038e54442033c2a3e5edc80b8237
Abdulkadir DILSIZ <akdilsiz@tecpor.com>
2024-07-24 20:06:45 +0300

    Version bumps

--- client.js
+++ client.js
@@ -24,7 +24,7 @@ export default class TCAbciClient {
   subscribed = false
   subscribedAddresses = []
   connected = false
-  version = 'v2.0.1'
+  version = 'v2.0.2'
   errorCb = null
   listenCb = null
   ws = null
