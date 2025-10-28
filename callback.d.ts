420627cb482d8f2799576a10554224bc7b4099ea
Abdulkadir DILSIZ <abdulkadir.dilsiz@transferchain.io>
2025-10-29 02:28:13 +0300

    General improvements and fix typescript definitions

---
+++ callback.d.ts
@@ -0,0 +1,4 @@
+export declare type SuccessCallback = (event: Event) => void
+export declare type ErrorCallback = (event: Event) => void
+export declare type CloseCallback = (event: Event) => void
+export declare type ListenCallback = (event: Event) => void
