aaab9bd301402cffd3e44d89bf153228d212a5d0
Abdulkadir DILSIZ <abdulkadir.dilsiz@transferchain.io>
2026-01-31 12:05:18 +0300

    Update

--- constants.js
+++ constants.js
@@ -1,5 +1,2 @@
-export const SUBSCRIBEMessage = 'subscribe',
-  UNSUBSCRIBEMessage = 'unsubscribe',
-  CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789',
-  READ_NODE_ADDRESS = 'https://test-read-node-01.transferchain.io',
-  READ_NODE_WS_ADDRESS = 'wss://test-read-node-01.transferchain.io/ws'
+export const READ_NODE_ADDRESS = 'https://read-node-01.transferchain.io',
+  READ_NODE_WS_ADDRESS = 'wss://read-node-01.transferchain.io/ws'
